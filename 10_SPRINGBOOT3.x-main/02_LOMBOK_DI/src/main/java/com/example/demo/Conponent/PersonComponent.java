03064c0758c1443df638d6e148f4f3656194b85d
byeongseon <byeongseon8@gmail.com>
2026-05-28 12:31:28 +0900

    2026. 05. 28. SPRINGBOOT3

---
+++ 10_SPRINGBOOT3.x-main/02_LOMBOK_DI/src/main/java/com/example/demo/Conponent/PersonComponent.java
@@ -0,0 +1,20 @@
+package com.example.demo.Conponent;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class PersonComponent {
+    private String name;
+    private int age;
+    private String addr;
+    PersonComponent(){
+        this.name = "티모";
+        this.age = 100;
+        this.addr = "창원";
+    }
+
+
+
+}
